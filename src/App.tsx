420369b6c1512fa697cd01dded9a746689b7e2d2
Piotr Malatyński <piotrmalatynski1@gmail.com>
2022-02-25 20:07:05 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import init, { add } from "wasm-lib";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState(0);
+
+  useEffect(() => {
+    init().then(() => console.log("wsam ready"));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{state}</div>
+      <button onClick={() => setState(add(state, 1))}>add</button>
     </div>
   );
 }
